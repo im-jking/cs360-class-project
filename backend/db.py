e70ad6ca5d60a2c20d2ed45abe884d3ecec97be8
im-jking <jamesianking@hotmail.com>
2025-03-14 21:18:46 -0700

    Initial DB work and attempting to connect things

---
+++ backend/db.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine, Table, Boolean, Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from pydantic import BaseModel
+
+URL_DATABASE = "mysql+mysqlconnector://class_host:360classproj@localhost:3306/360_class"
+
+engine = create_engine(URL_DATABASE, pool_size= 10, max_overflow= 30)
+
+local_session = sessionmaker(autocommit=False, autoflush=False, bind= engine)
+
+base = declarative_base()
+
+class User(base):
+    __tablename__ = 'users'
+
+    idusers = Column(Integer, primary_key=True, unique=True, index=True, autoincrement=True)
+    username = Column(String(45), primary_key=True, unique=True)
+    credit = Column(Integer)
+    is_admin = Column(Boolean)
+    password = Column(String(45), nullable=True)
+    phone_num = Column(String(45))
+    street_num = Column(String(45))
+    city = Column(String(45))
+    state = Column(String(45))
+    zip_code = Column(Integer)
+    email = Column(String(45))
+    is_approved = Column(Boolean)
+
+class Products(base):
+    __tablename__ = 'products'
+
+    idProducts = Column(Integer, primary_key=True, index=True, autoincrement=True, nullable=True, unique=True)
+    prodName =  Column(String(45))
+    prodDesc = Column(String(100))
+    price = Column(Integer)
+    datetime_created = Column(DateTime)
+    is_active = Column(Boolean)
+    is_exchanged = Column(Boolean)
+    posted_by = Column(Integer)
+
+class Transactions(base):
+    __tablename__ = 'transactions'
+
+    idtransactions = Column(Integer, primary_key=True, nullable=True, autoincrement=True)
+    item_exchanged_1 = Column(Integer)
+    item_exchanged_2 = Column(Integer)
+    party_1 = Column(Integer)
+    party_2 = Column(Integer)
+    date_started = Column(DateTime)
+    date_ended = Column(DateTime)
+    hash_key = Column(String(45), nullable=True, unique=True)
--- frontend/app/(tabs)/account.tsx
+++ frontend/app/(tabs)/account.tsx
@@ -1,6 +1,37 @@
 import { Text, View } from "react-native";
+import { Input } from "@rneui/themed";
 
 export default function Index() {
+  const register = async (
+    username: string,
+    password: string,
+    phoneNum: number,
+    streetNum: string,
+    city: string,
+    state: string,
+    zip: number,
+    email: string
+  ) => {
+    const loginInfo = await fetch("localhost:8000/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: `${username}`,
+        password: `${password}`,
+        phone_num: `${phoneNum}`,
+        street_num: `${streetNum}`,
+        city: `${city}`,
+        state: `${state}`,
+        zip_code: `${zip}`,
+        email: `${email}`,
+      }),
+    })
+      .then((response) => console.log(response))
+      .catch((error) => console.error("Registration error:" + error));
+  };
+
+  const login = (username: string, password: string) => {};
+
   return (
     <View
       style={{
@@ -9,7 +40,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello, account!</Text>
+      <Input></Input>
     </View>
   );
 }
